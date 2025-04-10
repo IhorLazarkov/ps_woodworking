1aa702ccae18e5e8abd60debb278bb6449030245
JamesDM62 <lsufan622000@yahoo.com>
2025-04-09 21:33:09 -0500

    added some constraints on email and password

--- backend/app/forms/signup_form.py
+++ backend/app/forms/signup_form.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Length
 from app.models import User
 
 
 def user_exists(form, field):
     # Checking if user exists
-    email = field.data
+    email = field.data.lower()
     user = User.query.filter(User.email == email).first()
     if user:
         raise ValidationError('Email address is already in use.')
@@ -22,6 +22,6 @@ def username_exists(form, field):
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+        'username', validators=[DataRequired(), username_exists, Length(min=4)])
+    email = StringField('email', validators=[DataRequired(), Email(), user_exists])
+    password = StringField('password', validators=[DataRequired(), Length(min=8)])
