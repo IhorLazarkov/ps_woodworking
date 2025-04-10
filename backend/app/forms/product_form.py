7c39a64640a9f929ade4b2e5c61aa82be16db58a
DoctorDoofen <and.bennett0930@gmail.com>
2025-04-09 22:18:11 -0500

    product form  basically done, just need to define the department choices

--- backend/app/forms/product_form.py
+++ backend/app/forms/product_form.py
@@ -1,4 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-# from wtforms.validators import
-from app.models import Products
+from wtforms import StringField, DecimalField, IntegerField, SelectField
+from wtforms.validators import DataRequired, ValidationError
+
+CHOICES = []
+class ProductForm(FlaskForm):
+
+    product_name = StringField('Product Name', validators=[DataRequired()])
+    product_price = DecimalField('Price', places=2, rounding=None, validators=[DataRequired()])
+    department = SelectField('Departmen', validators=[DataRequired()], choices=[CHOICES])
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()],)
+    
+    
