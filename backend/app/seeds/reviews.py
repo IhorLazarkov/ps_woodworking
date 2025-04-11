8878d20a45f074b8c0d55544e64e51e780bb5b44
ilazarkov <ilazarkov@gmail.com>
2025-04-10 21:03:04 -0500

    added seed

--- backend/app/seeds/reviews.py
+++ backend/app/seeds/reviews.py
@@ -1,7 +1,21 @@
-
+from app.models import db, User, Product, environment, SCHEMA
+from sqlalchemy.sql import text
 
 def seed_reviews():
-    pass
+    user = User.query.first()
+    product = Product.query.first()
+    Product(
+        user.id,
+        product.id,
+        rating = 4,
+        reviews = "This product is not so bad."
+    )
+
+    db.session.add(product)
+    db.session.commit()
 
 def undo_reviews():
-    pass
+    if environment == 'production':
+        db.session.execute(f"TRUNCATE table {SCHEMA}.reviews CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM reviews"))
