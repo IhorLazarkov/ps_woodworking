f9cfe96f9bd85ff67e54790e58c3a1b47d282f02
ilazarkov <ilazarkov@gmail.com>
2025-04-10 21:05:09 -0500

    added review seed

--- backend/app/seeds/reviews.py
+++ backend/app/seeds/reviews.py
@@ -1,17 +1,23 @@
-from app.models import db, User, Product, environment, SCHEMA
+from app.models import db, User, Product, Review, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_reviews():
     user = User.query.first()
-    product = Product.query.first()
-    Product(
+    #find a prodcur where user is not owner
+    product = Product.query.filter(Product.user_id != user.id).first()
+
+    if product is None:
+        print ("No product was found for user: " + user.to_dict())
+        return
+    
+    review = Review(
         user.id,
         product.id,
         rating = 4,
         reviews = "This product is not so bad."
     )
 
-    db.session.add(product)
+    db.session.add(review)
     db.session.commit()
 
 def undo_reviews():
@@ -19,3 +25,4 @@ def undo_reviews():
         db.session.execute(f"TRUNCATE table {SCHEMA}.reviews CASCADE;")
     else:
         db.session.execute(text("DELETE FROM reviews"))
+        
