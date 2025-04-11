713c2dbf3e8228d7da97d3402d051533d317a0d6
DoctorDoofen <and.bennett0930@gmail.com>
2025-04-10 19:44:28 -0500

    changes to field validations

--- backend/app/forms/product_form.py
+++ backend/app/forms/product_form.py
@@ -1,14 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DecimalField, IntegerField, SelectField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError, Length, NumberRange
 
-CHOICES = []
+
+
+CHOICES = ["Furniture", "Decorations", "Fun & Games"]
 class ProductForm(FlaskForm):
 
-    product_name = StringField('Product Name', validators=[DataRequired()])
-    product_price = DecimalField('Price', places=2, rounding=None, validators=[DataRequired()])
-    department = SelectField('Departmen', validators=[DataRequired()], choices=[CHOICES])
-    quantity = IntegerField('Quantity', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()],)
+    product_name = StringField('Product Name', 
+        validators=[
+            DataRequired("Product must have a name")
+            ])
+    product_price = DecimalField('Price', places=2, rounding=None, 
+        validators=[
+            DataRequired("Please enter a price greater than $0.00"),
+            NumberRange(min=0.01, message="price must be greater than 0")
+            ])
+    department = SelectField('Department', 
+        validators=[
+            DataRequired("Please specify a department for your product")
+            ], choices=[CHOICES])
+    quantity = IntegerField('Quantity', 
+        validators=[
+            DataRequired("Please enter a quantity"),
+            NumberRange(min=1, message="Quantity has to be greater than 0")
+            ])
+    description = StringField('Description', 
+        validators=[
+            DataRequired("Product must have a description"),
+            Length(min=50, max=255, message="Description needs to be at least 50 characters")
+            ])
     
     
