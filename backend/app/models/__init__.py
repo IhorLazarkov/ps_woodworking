d33b199b0b81a8ebe74e3058bec46818260b17a4
ilazarkov <ilazarkov@gmail.com>
2025-04-10 21:15:08 -0500

    made Order_Items singular in models/__init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,7 @@
 from .db import db
 from .user import User
 from .products import Product
-from .order_items import Order_Items
+from .order_items import Order_Item
 from .orders import Order
 from .reviews import Review
 from .db import environment, SCHEMA
