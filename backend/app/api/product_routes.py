e113f849eacbad451e0777011e329b5436b5f8dd
ilazarkov <ilazarkov@gmail.com>
2025-04-12 13:38:23 -0500

    added get products endpoint

---
+++ backend/app/api/product_routes.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import Product
+
+product_routes = Blueprint('products', __name__)
+
+@product_routes.route("/")
+def get_products():
+    products = Product.query.all()
+    return {'products': [product.to_dict() for product in products]}
