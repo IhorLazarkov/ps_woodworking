81de5b30aa0fe24640ec272008dfd5d2cc718796
DoctorDoofen <and.bennett0930@gmail.com>
2025-04-11 19:33:02 -0500

    edited user query to filter sellers

--- backend/app/seeds/products.py
+++ backend/app/seeds/products.py
@@ -5,12 +5,11 @@ from sqlalchemy.sql import text
 
 # Adds a demo product, you can add other products here if you want
 def seed_products():
-    user = User.query.first()
+    user = User.query.filter(User.seller == True).first()
     items = [
         Product(seller_id=user.id, product_name='Chess Board', product_price=10.99, department='Fun & Games', quantity=1, description='this game is so fun'),
         Product(seller_id=user.id, product_name='Cheese Board', product_price=15.99, department='Decorations', quantity=1, description='this cheese board is homemade'),
         Product(seller_id=user.id, product_name='Wooden Boat', product_price=10.99, department='Decorations', quantity=1, description='this Wooden Boat is well crafted'),
-
     ]
     
     
