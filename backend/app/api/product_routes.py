8f16e134ee4c033aecadb101566a1c51b6fd679b
ilazarkov <ilazarkov@gmail.com>
2025-04-12 17:21:29 -0500

    added delete product route. also added validations and added db.session.get(Product, id) to get product by its pk

--- backend/app/api/product_routes.py
+++ backend/app/api/product_routes.py
@@ -49,10 +49,49 @@ def delete_product(id):
     if not current_user.is_authenticated:
         return { 'errors': { "message": "Unauthorized" } }, 401
 
-    product = Product.query.filter(Product.id == id).first()
-    if product and product.seller_id == current_user.id:
+    product = db.session.get(Product, id)
+
+    if product is None:
+        return {"errors": { "message": "Product was not found" }}, 404
+    
+    if product.seller_id == current_user.id:
         db.session.delete(product)
         db.session.commit()
         return {"message":"success"}, 200
+
+    if product.seller_id != current_user.id:
+        return { "errors": { "message":"Unathorized to delete"}}, 401
         
     return {"errors": {"message": "failed"}}, 500
+
+@product_routes.route('/<int:id>', methods=['PUT'])
+def update_product(id):
+    """
+    Update Product
+    """
+    if not current_user.is_authenticated:
+        return { 'errors': { "message": "Unauthorized"} }, 401
+
+    form = ProductForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        product = db.session.get(Product, id)
+
+        if product is None:
+            return {"errors": { "message": "Product was not found" }}, 404
+    
+        if product.seller_id == current_user.id:
+            product.product_name = form.data["product_name"]
+            product.product_price = form.data["product_price"]
+            product.department = form.data["department"]
+            product.quantity = form.data["quantity"]
+            product.description = form.data["description"]
+            
+            db.session.add(product)
+            db.session.commit()
+            return {"message": "success"}, 200
+
+        if product.seller_id != current_user.id:
+            return { "errors": { "message":"Unathorized to update"}}, 401
+    
+    return {"errors": { "message":"failed" }}, 500
