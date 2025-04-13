f7d381ef89b2b92beb33218cfaf67d8c67905cf9
ilazarkov <ilazarkov@gmail.com>
2025-04-13 15:54:19 -0500

    added stats such as numOfRatings and avarage rating and fix an issue of avgRating when there are no review for get product details endpoint

--- backend/app/api/session_routes.py
+++ backend/app/api/session_routes.py
@@ -1,11 +1,30 @@
+from sqlalchemy import func
 from flask import Blueprint
 from flask_login import login_required, current_user
-from app.models import Product
+from app.models import db, Product, Review
 
 session_routes = Blueprint('sessions', __name__)
 
 @session_routes.route("/products")
 @login_required
 def get_current_user_products():
-    products = Product.query.filter(Product.seller_id == current_user.id).all()
-    return {"products": [product.to_dict() for product in products]}, 200
+    """
+    Get all products of current user
+    """
+    response = []
+    products = db.session.query(Product).filter_by(seller_id = current_user.id).all()
+
+    for product in products:
+        if product is not None:
+            result = product.to_dict()
+            stats = db.session.query(
+                Review,
+                func.count(Review.id).label("numOfReviews"),
+                func.sum(Review.rating).label("totalRating")
+            ).filter_by(product_id = product.id).first()
+            if stats["numOfReviews"] is not 0:
+                result["avgRating"] = stats["totalRating"] / stats["numOfReviews"]
+            else: result["avgRating"] = 0
+            response.append(result)
+
+    return {"products": response}, 200
