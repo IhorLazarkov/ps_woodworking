1d72d9854f56e599d64baae88b6f7e3572b20633
ilazarkov <ilazarkov@gmail.com>
2025-04-17 21:22:46 -0500

    added footer and added layout for main section

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,25 +1,42 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchProducts } from "../../redux/products"
 import ProductCard from "../ProductCard"
 import { Link } from "react-router-dom"
+import "./LandingPage.css"
+import FooterCard from "./FooterCard"
 
 function LandingPage() {
     const dispatch = useDispatch()
     const products = useSelector(state => state.products.products)
+    const [isLoaded, setIsLoaded] = useState(false)
 
     useEffect(() => {
-        dispatch(fetchProducts()).then()
+        dispatch(fetchProducts()).then(() => setIsLoaded(true))
     }, dispatch)
 
     return (
         <>
             <h1>Landing page</h1>
-            {products && products.map(p => {
-                return <Link to={`product/${p.id}`}> 
-                    < ProductCard key = {p.id} product={p}/>
-                </Link>
-            })}
+            <main id="landing-page">
+                {!isLoaded
+                    ? <h3>Loading ...</h3>
+                    : <>
+                        {products && products.map(p => {
+                            return <Link to={`product/${p.id}`}>
+                                < ProductCard key={p.id} product={p} />
+                            </Link>
+                        })}
+                    </>
+                }
+            </main>
+            <footer id="landing-page">
+                <FooterCard />
+                <div className="delimeter"></div>
+                <FooterCard />
+                <div className="delimeter"></div>
+                <FooterCard />
+            </footer>
         </>
     )
 }
