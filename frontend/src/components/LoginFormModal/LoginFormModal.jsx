b3e6581eeb9417bd63891633c2d2b87505176d56
JamesDM62 <lsufan622000@yahoo.com>
2025-04-18 21:05:13 -0500

    login as demo

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -28,6 +28,23 @@ function LoginFormModal() {
     }
   };
 
+  const handleDemoLogin = async (e) => {
+    e.preventDefault();
+
+    const serverResponse = await dispatch(
+      thunkLogin({
+        email: "demo@aa.io",
+        password: "password",
+      })
+    );
+
+    if (serverResponse) {
+      setErrors(serverResponse);
+    } else {
+      closeModal();
+    }
+  };
+
   return (
     <>
       <h1>Log In</h1>
@@ -53,6 +70,11 @@ function LoginFormModal() {
         </label>
         {errors.password && <p>{errors.password}</p>}
         <button type="submit">Log In</button>
+        <button type="submit">Log In</button>
+        <button type="button" onClick={handleDemoLogin} className="demo-login-btn">
+          Log In as Demo User
+        </button>
+
       </form>
     </>
   );
