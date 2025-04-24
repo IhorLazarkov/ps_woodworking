0585c288f779063d4a92049833de6c5ef6434bcd
ilazarkov@gmail.com <dev@MacMini.attlocal.net>
2025-04-23 21:42:39 -0500

    config fixes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -15,7 +15,7 @@ from .api.orders_route import orders_routes
 from .seeds import seed_commands
 from .config import Config
 
-app = Flask(__name__, static_folder='../react-vite/dist', static_url_path='/')
+app = Flask(__name__, static_folder='../../frontend/dist', static_url_path='/')
 
 # Setup login manager
 login = LoginManager(app)
